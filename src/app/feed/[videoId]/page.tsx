49f8dfd5c9b62d9be881d12a448c6ceb90df528e
Shen Shining <353824385@qq.com>
2025-03-12 22:24:16 +0800

    Preliminary completion of the navbar and sidebar framework for the homepage

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,12 @@
+interface PageProps {
+    params: Promise<{ videoId: string }>
+}
+
+export default async function Page({ params }: PageProps) {
+    const { videoId } = await params
+    return (
+        <div>
+            Video Id: {videoId}
+        </div>
+    )
+}
