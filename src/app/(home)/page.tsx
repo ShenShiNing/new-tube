c1408ba99251a67646f90221c55810db7f6a5a50
Shen Shining <353824385@qq.com>
2025-03-13 21:04:53 +0800

    Enable transformer on tRPC Add auth to tRPC context Add protectedProcedue Add rate limiting

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,18 @@
-import { trpc } from "@/trpc/server"
+import { HydrateClient, trpc } from "@/trpc/server"
+import { PageClient } from "./client"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 export default async function Home() {
-  const data = await trpc.hello({ text: 'ShenShining' })
-  
+  await trpc.hello.prefetch({ text: 'ShenShining' })
+
   return (
-    <div>
-      Client components says: { data.greeting }
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <PageClient />
+      </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   )
 }
--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -7,6 +7,7 @@ import { createTRPCReact } from '@trpc/react-query'
 import { useState } from 'react'
 import { makeQueryClient } from "./query-client"
 import type { AppRouter } from './routers/_app'
+import superjson from 'superjson'
 
 export const trpc = createTRPCReact<AppRouter>()
 
@@ -45,7 +46,7 @@ export function TRPCProvider(
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    // transformer: superjson,
+                    transformer: superjson,
                     url: getUrl()
                 })
             ]
