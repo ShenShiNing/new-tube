0fd7ecfd62f6c97d6c1f918436b2398385b2848e
Shen Shining <353824385@qq.com>
2025-03-16 20:17:48 +0800

    subscriptions

---
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -0,0 +1,71 @@
+import { trpc } from "@/trpc/client";
+import { toast } from "sonner";
+import { useClerk } from "@clerk/nextjs";
+
+interface UseSubscriptionProps {
+    userId: string,
+    isSubscribed: boolean,
+    formVideoId?: string
+}
+
+export const useSubscription = ({
+    userId,
+    isSubscribed,
+    formVideoId
+}: UseSubscriptionProps) => {
+    const clerk = useClerk()
+    const utils = trpc.useUtils()
+
+    const subscribe = trpc.subscriptions.create.useMutation({
+        onSuccess: () => {
+            toast.success("Subscribed")
+            // TODO: reinvalidate subscriptions.getMany, users.getOne
+
+            if (formVideoId) {
+                utils.videos.getOne.invalidate({ id: formVideoId })
+            }
+        },
+        onError: (error) => {
+            if (error.data?.code === "UNAUTHORIZED") {
+                toast.error("Please login to subscribe")
+                clerk.openSignIn()
+            } else {
+                toast.error("Something went wrong")
+            }
+            
+        }
+    })
+    const unSubscribe = trpc.subscriptions.remove.useMutation({
+        onSuccess: () => {
+            toast.success("Unsubscribed")
+            // TODO: reinvalidate subscriptions.getMany, users.getOne
+
+            if (formVideoId) {
+                utils.videos.getOne.invalidate({ id: formVideoId })
+            }
+        },
+        onError: (error) => {
+            if (error.data?.code === "UNAUTHORIZED") {
+                toast.error("Please login to unsubscribe")
+                clerk.openSignIn()
+            } else {
+                toast.error("Something went wrong")
+            }
+        }
+    })
+
+    const isPending = subscribe.isPending || unSubscribe.isPending
+
+    const onClick = () => {
+        if (isSubscribed) {
+            unSubscribe.mutate({ userId })
+        } else {
+            subscribe.mutate({ userId })
+        }
+    }
+
+    return {
+        isPending,
+        onClick
+    }
+}
--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -4,6 +4,7 @@ import { UserAvatar } from "@/components/user-avatar"
 import { Button } from "@/components/ui/button"
 import { useAuth } from "@clerk/nextjs"
 import { UserInfo } from "@/modules/users/ui/components/user-info"
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription"
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button"
 
 interface VideoOwnerProps {
@@ -12,7 +13,12 @@ interface VideoOwnerProps {
 }
 
 export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
-    const { userId: clerkUserId } = useAuth()
+    const { userId: clerkUserId, isLoaded } = useAuth()
+    const { isPending, onClick } = useSubscription({
+        userId: user.id,
+        isSubscribed: user.viewerSubscribed,
+        formVideoId: videoId
+    })
 
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
@@ -22,8 +28,7 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                     <div className="flex flex-col min-w-0">
                         <UserInfo name={user.name} size="lg" />
                         <span className="text-sm text-muted-foreground line-clamp-1">
-                            {/* TODO: Properly fill subscribers count */}
-                            {0} subscribers
+                            {user.subscriberCount} subscribers
                         </span>
                     </div>
                 </div>
@@ -40,9 +45,9 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                 </Button>
             ) : (
                 <SubscriptionButton
-                    onClick={() => { }}
-                    disabled={false}
-                    isSubscribed={false}
+                    onClick={onClick}
+                    disabled={isPending || !isLoaded}
+                    isSubscribed={user.viewerSubscribed}
                     className="flex-none"
                 />
             )}
